332cb1c735d034fa6967a66a2cc21352fe082234
nelsonm04 <nelsonm43@icloud.com>
2026-03-11 18:05:41 -0400

    bug fix

--- FitnessApp/FitnessAppApp.swift
+++ FitnessApp/FitnessAppApp.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import SwiftData
 
 @main
 struct FitnessAppApp: App {
@@ -14,21 +13,10 @@ struct FitnessAppApp: App {
     @State private var appState = AppState()
     let exerciseService = ExerciseService()
 
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([Item.self])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
     var body: some Scene {
         WindowGroup {
             SplashView()
                 .environment(appState)
         }
-        .modelContainer(sharedModelContainer)
     }
 }
